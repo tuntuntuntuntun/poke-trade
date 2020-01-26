060e1437b4b35e52e351f7249ad281359683f1d3
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-01-26 11:26:31 +0900

    レイアウトを変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,23 +3,17 @@
 @section('content')
     <main>
         <div class="container">
-            <nav class="card">
-                <div class="card-body">
-                    <h2 class="card-title">投稿の一覧</h2>
-                    <div class="list-group">
-                        @foreach($posts as $post)
-                            <div class="list-group-item">
-                                <ul>
-                                    <li>欲しいポケモン: {{ $post->want }}</li>
-                                    <li>譲るポケモン: {{ $post->give }}</li>
-                                </ul>
-                            </div>
-                        @endforeach
+            <h2 class="card-title">投稿の一覧</h2>
+            <div class="list-group">
+                @foreach($posts as $post)
+                    <div class="list-group-item">
+                        <ul>
+                            <li>欲しいポケモン: {{ $post->want }}</li>
+                            <li>譲るポケモン: {{ $post->give }}</li>
+                        </ul>
                     </div>
-                </div>
-            </nav>
+                @endforeach
+            </div>
         </div>
     </main>
 @endsection
-
-<!-- 投稿作成機能を作ろう -->
