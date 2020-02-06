751bdcd4d41a9768c18cd0b6034fae84c89e5ab4
tuntuntuntuntun <tuntuntun02b@gmail.com>
2020-02-07 05:04:47 +0900

    mainタグを追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,27 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
-        @if($errors->any())
-            <div class="alert alert-danger">
-                @foreach($errors->all() as $message)
-                    <p>{{ $message }}</p>
-                @endforeach
-            </div>
-        @endif
-        <form action="{{ route('posts.edit', ['post' => $post->id]) }}" method="post">
-            @csrf
-            <div class="form-group">
-                <label for="want">欲しいポケモン</label>
-                <input type="text" id="want" name="want" class="form-control" value="{{ old('want') ?? $post->want }}">
-            </div>
-            <div class="form-group">
-                <label for="give">譲るポケモン</label>
-                <input type="text" id="give" name="give" class="form-control" value="{{ old('give') ?? $post->give }}">
-            </div>
-            <div class="text-right">
-                <button type="submit" class="btn btn-primary">変更する</button>
-            </div>
-        </form>
-    </div>
+    <main>
+        <div class="container">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    @foreach($errors->all() as $message)
+                        <p>{{ $message }}</p>
+                    @endforeach
+                </div>
+            @endif
+            <form action="{{ route('posts.edit', ['post' => $post->id]) }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="want">欲しいポケモン</label>
+                    <input type="text" id="want" name="want" class="form-control" value="{{ old('want') ?? $post->want }}">
+                </div>
+                <div class="form-group">
+                    <label for="give">譲るポケモン</label>
+                    <input type="text" id="give" name="give" class="form-control" value="{{ old('give') ?? $post->give }}">
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">変更する</button>
+                </div>
+            </form>
+        </div>
+    </main>
 @endsection
